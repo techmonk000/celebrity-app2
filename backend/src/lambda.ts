b867d012a37700a663aa5164dbeeb43e2a99111a
techmonk000 <swarnavomukherjee03@gmail.com>
2025-07-05 23:25:20 +0530

    fixed

--- backend/src/lambda.ts
+++ backend/src/lambda.ts
@@ -1,24 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-misused-promises */
-import { Handler } from 'aws-lambda';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
 import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 
 let cachedServer: Handler;
 
-async function bootstrap(): Promise<Handler> {
-  const expressApp = express();
-  const adapter = new ExpressAdapter(expressApp);
-  const app = await NestFactory.create(AppModule, adapter);
-  app.enableCors();
-  await app.init();
-  return serverlessExpress({ app: expressApp });
+async function bootstrapServer(): Promise<Handler> {
+  if (!cachedServer) {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    await app.init();
+    const expressApp = app.getHttpAdapter().getInstance();
+    cachedServer = serverlessExpress({ app: expressApp });
+  }
+  return cachedServer;
 }
 
-export const handler: Handler = async (event, context, callback) => {
-  cachedServer = cachedServer ?? (await bootstrap());
-  return cachedServer(event, context, callback);
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  const server = await bootstrapServer();
+  return server(event, context, callback);
 };
